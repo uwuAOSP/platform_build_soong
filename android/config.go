sileshn/android_build_soong

soong: Treat vendor/lineage-priv/* as release-keys
Change-Id: I7e7f180dda4402ada8b2124d0c26bb844dc2cb5f

android/config.go
@@ -1056,6 +1056,9 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
+	if strings.HasPrefix(defaultCert, "vendor/lineage-priv/") {
+		return "release-keys"
+	}
 	return "dev-keys"
 }
 

scripts/gen_build_prop.py
@@ -43,6 +43,8 @@ def get_build_keys(product_config):
   default_cert = product_config.get("DefaultAppCertificate", "")
   if default_cert == "" or default_cert == os.path.join(TEST_KEY_DIR, "testKey"):
     return "test-keys"
+  if default_cert.startswith("vendor/lineage-priv/"):
+    return "release-keys"
   return "dev-keys"
 
 def override_config(config):

uwuAOSP/platform_build_soong

android/config.go
@@ -1525,6 +1525,9 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
+	if strings.HasPrefix(defaultCert, "vendor/uwu-priv/") {
+		return "release-keys"
+	}
 	return "dev-keys"
 }
 

scripts/gen_build_prop.py
@@ -43,6 +43,8 @@ def get_build_keys(product_config):
   default_cert = product_config.get("DefaultAppCertificate", "")
   if default_cert == "" or default_cert == os.path.join(TEST_KEY_DIR, "testKey"):
     return "test-keys"
+  if default_cert.startswith("vendor/uwu-priv/"):
+    return "release-keys"
   return "dev-keys"
 
 def override_config(config):