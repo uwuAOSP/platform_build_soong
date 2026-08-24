665ede8f624c1984ca74d30ec7a2aa186183426f
Jelly10086 <Oo40469600@outlook.com>
2026-08-24 15:02:08 +0800

    soong: Disable incremental analysis for uni prepare

--- ui/build/config.go
+++ ui/build/config.go
@@ -1523,6 +1523,8 @@ func (c *configImpl) SetSkipNinja(v bool) {
 func (c *configImpl) SetUniPrepareMode() {
 	c.uniPrepareMode = true
 	c.skipNinja = true
+	c.incrementalBuildActions = false
+	c.incrementalBuildActionsSetInEnv = true
 }
 
 func (c *configImpl) UniPrepareMode() bool {
